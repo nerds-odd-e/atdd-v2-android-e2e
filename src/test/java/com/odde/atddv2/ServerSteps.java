087c34eb92bbc615ff892f5fb787eb65324e5b69
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-08-22 15:59:41 +0800

    :sparkles: provide all furniture chair products from mock server with param

--- src/test/java/com/odde/atddv2/ServerSteps.java
+++ src/test/java/com/odde/atddv2/ServerSteps.java
@@ -3,14 +3,12 @@ package com.odde.atddv2;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.zh_cn.假如;
-import io.cucumber.java.zh_cn.当;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class ServerSteps {
 
@@ -25,12 +23,6 @@ public class ServerSteps {
     public void 存在商品类别(DataTable dataTable) {
         String response = objectMapper.writeValueAsString(dataTable.asMaps());
         mockServer.getJson("/product-categories", response);
-    }
-
-    @SneakyThrows
-    @当("启动服务器")
-    public void 启动服务器() {
-        TimeUnit.DAYS.sleep(2);
     }
 
     @SneakyThrows
